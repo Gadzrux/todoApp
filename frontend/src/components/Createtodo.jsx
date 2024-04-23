fc7a74073e2502dce4d55cd0013b597c8aa9ba50
Gadzrux <krishnavaghosh23@gmail.com>
2024-04-23 13:19:04 +0530

    fixing bugs

---
+++ frontend/src/components/Createtodo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Createtodo() {
+  return (
+    <>
+      <h1>Create Todo</h1>
+      <input type="text" placeholder="title"></input>
+      <input type="text" placeholder="decription"></input>
+      <br></br>
+      <button>Add a todo</button>
+    </>
+  );
+}
+
+export default Createtodo;
