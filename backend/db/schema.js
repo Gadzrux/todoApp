230fbf414066b40c8373f5d689612bc3cecb535e
Gadzrux <krishnavaghosh23@gmail.com>
2024-04-23 12:08:23 +0530

    backend completed

---
+++ backend/db/schema.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { boolean } from "zod";
+
+const URL =
+  "mongodb+srv://admin:Charizard14@cluster0.0f54goo.mongodb.net/todoApp";
+mongoose.connect(URL);
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const Todo = mongoose.model("Todo", todoSchema);
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,24 @@
 import express from "express";
 import { createTodo, updateTodo } from "./zod/validationSchema";
+import { Todo } from "./db/schema";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/todos", (req, res) => {
+app.get("/todos", async (req, res) => {
   //get all todos in the db
+  try {
+    const allTodos = await Todo.find({});
+    res.status(200).json({ allTodos });
+  } catch (error) {
+    res.status(404).json({
+      message: "some Error occured or DB is down",
+    });
+  }
 });
 
-app.post("/todo", (req, res) => {
+app.post("/todo", async (req, res) => {
   //post todos here
   const bodyObj = req.body;
   const zodObj = createTodo.safeParse(bodyObj);
@@ -18,6 +27,17 @@ app.post("/todo", (req, res) => {
       message: "Todo created successfully",
       created: true,
     });
+    try {
+      await Todo.create({
+        title: bodyObj.title,
+        description: bodyObj.description,
+        completed: false,
+      });
+    } catch (error) {
+      res.status(400).json({
+        message: "some error occured or DB is down",
+      });
+    }
   } else {
     res.status(411).json({
       message: "wrong inputs passed in the body",
@@ -27,8 +47,9 @@ app.post("/todo", (req, res) => {
   }
 });
 
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
   //update existing todo with todoId
+  //zod validation
   const bodyObj = req.body;
   const zodObj = updateTodo.safeParse(bodyObj);
   if (zodObj.success) {
@@ -36,7 +57,26 @@ app.put("/completed", (req, res) => {
       message: "Todo updated successfully",
       created: true,
     });
+    // if the zod validation passes we update the todo in the db
+    try {
+      await Todo.findOneAndUpdate(
+        {
+          _id: bodyObj.id, //filter
+        },
+        {
+          completed: true, //update
+        }
+      );
+      res.status(200).json({
+        message: "Todo marked as completed",
+      });
+    } catch (error) {
+      res.status(404).json({
+        message: "DB is down",
+      });
+    }
   } else {
+    //if zod validation fails
     res.status(411).json({
       message: "wrong inputs passed in the body",
       created: false,
